9803eab3b61f978ebca3cbd4150e8f918a7a0b49
Philainel <toropovmat@gmail.com>
2023-11-19 19:24:27 +0500

    OpenGraph test and twitch link

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -18,40 +18,48 @@ function Root() {
 		},
 		{
 			id: "2",
+			text: "Reddit",
+			href: "https://reddit.com/u/Philainel",
+			icon: "bi-reddit",
+			accentColor: "#FF5700"
+		},
+		{
+			id: "3",
 			text: "Spotify",
 			href: "https://open.spotify.com/user/zzizbtt9g6zynmcu7wuj5d5p5?si=a5f610866a174fa9",
 			icon: "bi-spotify",
 			accentColor: "#1DB954"
 		},
 		{
-			id: "3",
+			id: "4",
 			text: "Steam",
 			href: "https://steamcommunity.com/id/Philainel/",
 			icon: "bi-steam",
 			accentColor: "#1b2838"
 		},
 		{
-			id: "4",
-			text: "YouTube",
-			href: "https://youtube.com/@philainel",
-			icon: "bi-youtube",
-			accentColor: "#FF0000"
+			id: "5",
+			text: "Twitch",
+			href: "https://twitch.tv/philainel",
+			icon: "bi-twitch",
+			accentColor: "#6441A5"
 		},
 		{
-			id: "5",
+			id: "6",
 			text: "Twitter",
 			href: "https://twiiter.com/philainel",
 			icon: "bi-twitter",
 			accentColor: "#1DA1F2"
 		},
 		{
-			id: "6",
-			text: "Reddit",
-			href: "https://reddit.com/u/Philainel",
-			icon: "bi-reddit",
-			accentColor: "#FF5700"
+			id: "7",
+			text: "YouTube",
+			href: "https://youtube.com/@philainel",
+			icon: "bi-youtube",
+			accentColor: "#FF0000"
 		}
 	]
+	// .sort((l1, l2) => l1.text.localeCompare(l2.text))
 	return (
 		<div id="RootPage">
 			<div className="block">
@@ -64,7 +72,7 @@ function Root() {
 				<div className="links">
 					{links.map(l =>
 						<a href={l.href} className="link" target="_blank" id={l.id}>
-							<i className={l.icon} style={{color: l.accentColor}} />
+							<i className={l.icon} style={{color: l.accentColor}}/>
 							<span className="desc">{l.text}</span>
 						</a>
 					)}
