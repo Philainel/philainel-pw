65745d674e6241969e727ea72d60ce5b2a17dc23
Philainel <toropovmat@gmail.com>
2024-01-27 22:24:17 +0300

    Full tailwind + keys + mobile redesign

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,5 +1,3 @@
-import './Root.scss'
-
 function Root() {
 	let links = [
 		{
@@ -62,16 +60,20 @@ function Root() {
 	// .sort((l1, l2) => l1.text.localeCompare(l2.text))
 	return (
 		<div id="RootPage" className="flex flex-col gap-2">
-			<div className="bg-backgeoundImage-t500 rounded-2xl text-white font-medium backdrop-blur-[100px] py-6 px-5 roundex-2xl">
+			<div
+				className="bg-backgeoundImage-t500 rounded-2xl text-white font-medium backdrop-blur-[100px] py-6 px-5 roundex-2xl">
 				<span className="text-3xl font-bold drop-shadow-lg">Привет.</span><br/>
 				<span className="drop-shadow-lg">Меня зовут Матвей, но многие могут знать меня под ником Philainel. Здесь ты можешь найти ссылки на некоторые мои профили</span>
 			</div>
-			<div className="bg-backgeoundImage-t500 rounded-2xl text-white font-medium backdrop-blur-[100px] py-6 px-5 roundex-2xl">
+			<div
+				className="bg-backgeoundImage-t500 rounded-2xl text-white font-medium backdrop-blur-[100px] py-6 px-5 roundex-2xl">
 				<span className="text-3xl font-bold drop-shadow-lg">Ссылки:</span><br/>
 				<span className="drop-shadow-lg">Мои личные профили:</span>
-				<div className="grid grid-cols-5 gap-2 items-center mt-2 select-none">
+				<div className="grid grid-cols-2 gap-2 items-center mt-2 select-none md:grid-cols-5 sm:grid-cols-3">
 					{links.map(l =>
-						<a href={l.href} className="text-2xl bg-white py-1 px-2 flex items-center gap-1 rounded-2xl translate-y-0 hover:-translate-y-1 transition-transform duration-200 ease-in-out" target="_blank" id={l.id}>
+						<a href={l.href}
+						   className="text-2xl bg-white py-1 px-2 flex items-center gap-1 rounded-2xl translate-y-0 hover:-translate-y-1 transition-transform duration-200 ease-in-out"
+						   target="_blank" id={l.id}>
 							<i className={`${l.icon} text-3xl`} style={{color: l.accentColor}}/>
 							<span className="text-xl text-backgroundImage-500">{l.text}</span>
 						</a>
